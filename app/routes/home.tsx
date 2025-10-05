c8f0cf5c798921f3222af7d9e51795f6ab3a608f
Prince Chouhan <princechouhan4606@gmail.com>
2025-10-05 23:55:15 +0530

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,54 @@
+import Navbar from "~/Components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "Constants";
+import ResumeCard from "../Components/ResumeCard";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+import { useEffect } from "react";
+import auth from "./auth";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Resumind is a tool that helps you analyze your resume and get feedback on your resume." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { auth: { isAuthenticated } } = usePuterStore();
+
+  useEffect(() => {
+    if(!isAuthenticated) navigate('/auth?next=/');
+  }, [isAuthenticated , navigate])
+  
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+
+        {/* Render ResumeCards if there are resumes */}
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {/* Render ResumeCards */}
+            {resumes.map((resume) => (
+              <div key={resume.id}>
+                <ResumeCard key={resume.id} resume={resume} />
+              </div>
+            ))}
+          </div>
+        )}
+      </section>
+
+
+
+    </main>
+  );
 }
